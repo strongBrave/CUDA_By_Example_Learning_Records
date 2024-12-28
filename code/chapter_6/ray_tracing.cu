2ff72f5c87d2efeb2621597eed35c03c749e7295
junhaoyu <yu1791046157@gmail.com>
2024-12-28 23:13:46 +0800

    add chapter_6: constant memory

---
+++ code/chapter_6/ray_tracing.cu
@@ -0,0 +1,119 @@
+// This simplified ray tracing implementation is to understand how to use constant memory in GPU
+#include "cuda.h"
+#include "../common/book.h"
+#include "../common/cpu_bitmap_save.h"
+
+#define INF 2e10f
+#define DIM 512
+#define rnd(x) (x * rand() / RAND_MAX)
+#define SPHERES 1000 // If you want to see preformance improvement using constant memory, you should adjust ther number based on you device.
+
+struct Sphere
+{
+    float r, b, g;
+    float radius;
+    float x, y, z;
+
+    __device__ float hit(float ox, float oy, float *n)
+    {
+        float dx = ox - x;
+        float dy = oy - y;
+        if (dx * dx + dy * dy < radius * radius)
+        {
+            float dz = sqrtf(radius * radius - dx * dx - dy * dy);
+            *n = dz / sqrtf(radius * radius);
+            return dz + z;
+        }
+        return -INF;
+    }
+};
+
+__global__ void kernel(Sphere *s,  unsigned char *ptr)
+{
+    int x = threadIdx.x + blockDim.x * blockIdx.x;
+    int y = threadIdx.y + blockDim.y * blockIdx.y;
+    int offset = x + y * gridDim.x * blockDim.x;
+    float ox = (x - DIM / 2);
+    float oy = (y - DIM / 2);
+    
+    float r = 0, g = 0, b = 0;
+    float maxz = -INF;
+    for (int i = 0; i < SPHERES; i ++ )
+    {
+        float n;
+        float t = s[i].hit(ox, oy, &n);
+        if (t > maxz)
+        {
+            maxz = t;
+            float fscale = n;
+            r = s[i].r * fscale;
+            g = s[i].g * fscale;
+            b = s[i].b * fscale;
+        }
+    }
+    ptr[offset * 4 + 0] = (int)(r * 255);
+    ptr[offset * 4 + 1] = (int)(g * 255);
+    ptr[offset * 4 + 2] = (int)(b * 255);
+    ptr[offset * 4 + 3] = 255;
+}
+
+int main(void)
+{
+    // capture the start time
+    cudaEvent_t start, stop;
+    HANDLE_ERROR(cudaEventCreate(&start));
+    HANDLE_ERROR(cudaEventCreate(&stop));
+    HANDLE_ERROR(cudaEventRecord(start, 0));
+
+    CPUBitmap bitmap(DIM, DIM);
+    unsigned char *dev_bitmap;
+    Sphere *s;
+
+    // allocate memory on the GPU fot the output bitmap
+    HANDLE_ERROR(cudaMalloc((void**)&dev_bitmap, bitmap.image_size()));
+    
+    // allocate memory for the Sphere dataset
+    HANDLE_ERROR(cudaMalloc((void**)&s, sizeof(Sphere) * SPHERES));
+
+    // allocate temp memory, initialize it, copy to
+    // memory on the GPU, and then free our temp memory
+    Sphere *temp_s = (Sphere *)malloc(SPHERES * sizeof(Sphere));
+    for (int i = 0; i < SPHERES; i ++ )
+    {
+        temp_s[i].r = rnd(1.0f);
+        temp_s[i].g = rnd(1.0f);
+        temp_s[i].b = rnd(1.0f);
+        temp_s[i].x = rnd(1000.0f) - 500;
+        temp_s[i].y = rnd(1000.0f) - 500;
+        temp_s[i].z = rnd(1000.0f) - 500;
+        temp_s[i].radius = rnd(100.0f) + 20;
+    }
+
+    HANDLE_ERROR(cudaMemcpy(s, temp_s, sizeof(Sphere) * SPHERES, cudaMemcpyHostToDevice));
+    free(temp_s);
+
+    // generate a bitmap for our sphere data
+    dim3 grids(DIM / 16, DIM / 16);
+    dim3 threads(16, 16);
+    kernel<<<grids, threads>>>(s, dev_bitmap);
+
+    HANDLE_ERROR(cudaMemcpy(bitmap.get_ptr(), dev_bitmap, bitmap.image_size(), cudaMemcpyDeviceToHost));
+
+    // get stop time, and display the timing results
+    HANDLE_ERROR(cudaEventRecord(stop, 0));
+    HANDLE_ERROR(cudaEventSynchronize(stop));
+
+    float elapsedTime;
+    HANDLE_ERROR(cudaEventElapsedTime(&elapsedTime, start, stop));
+    printf("Time to generate: %3.1f ms\n", elapsedTime);
+
+    HANDLE_ERROR(cudaEventDestroy(start));
+    HANDLE_ERROR(cudaEventDestroy(stop));
+    
+    bitmap.display_and_exit();
+
+    // free our memory
+    cudaFree(dev_bitmap);
+    cudaFree(s);
+    
+}
